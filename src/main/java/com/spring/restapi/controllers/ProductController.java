0d05ad8d4d272ee0229a140d7589e1b732e44939
saikishore0305 <saikishore0305@gamil.com>
2018-06-12 20:38:30 +0530

    updated controller file for swagger 3.0

--- src/main/java/com/spring/restapi/controllers/ProductController.java
+++ src/main/java/com/spring/restapi/controllers/ProductController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.*;
  *
  * @author didin
  */
-@Api(basePath = "swagger-demo/person", value = "Person", description = "Operations with person", produces = "application/json")
+@Api(basePath = "/products", value = "Products", description = "Operations with Products", produces = "application/json")
 @RestController
 public class ProductController {
     
@@ -76,21 +76,5 @@ public class ProductController {
         productRepository.delete(product);
 
         return "product deleted";
-    }
-
-   @RequestMapping(value = "swagger-demo/person", produces = MediaType.APPLICATION_JSON_VALUE)
-   class PersonController {
- 
-    @RequestMapping(method = RequestMethod.POST, value = "/create")
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ApiOperation(value = "Get Person", notes = "Fetch List of Person")
-    @ApiResponses(value = 
-            @ApiResponse(code = 400, message = "Fields are with validation errors"),
-            @ApiResponse(code = 201, message = "List"),
-            @ApiResponse(code = 500, message = "Error occurred while fetching Person")
-    )
-    ResponseEntity create(@RequestBody PersonCO co) {
-        return new ResponseEntity(new Person(firstName: co.firstName, lastName: co.lastName, age: co.age), HttpStatus.OK)
-    }
     }
 }
